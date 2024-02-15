46ae8f1de9dff4529ea72fa3f76c3d51b4a6dec6
erenyenigul <tc.eren.yenigul@gmail.com>
2024-02-15 15:53:47 +0300

    feat: ir class

--- ssvm/ir.py
+++ ssvm/ir.py
@@ -1,31 +1,45 @@
-from action import Action
-import os
+from noinput import ContinueNoInputBehavior
 
 class SSVMIR:
-     
+    """
+    This class represents the SSVM Intermediate Representation.
+    It abstracts iOS/iPadOS/macOS version related details and provides a high level interface.
+    The compiler will take an instance of this class and compile it into a .plist file.
+    """
+
     def __init__(self) -> None :
-        self.actions = []
-        self.name = ""
+        self._actions = []
+        self._name = ""
+        self._no_input_behavior = ContinueNoInputBehavior()
+
+    @property
+    def name(self):
+        """
+        This is the name of the Shortcut. It will be displayed in the Shortcuts app.
+        This propery modifies the WFWorkflowName key in the compiled .plist file.
+        """
+        return self._name
+
+    @property.setter
+    def name(self, name):
+        self._name = name
+
+    @property
+    def no_input_behavior(self):
+        """
+        No input behavior is what happens when the Shortcut is run without input.
+        This property modifies the WFWorkflowNoInputBehavior key in the compiled .plist file.
+        """
+        return self._no_input_behavior
+    
+    @property.setter
+    def no_input_behavior(self, behavior):
+        self._no_input_behavior = behavior
 
     def save(self, path):
-        with open(path, "w") as f:
-            f.write("(\n")
-            f.write("\tversion 17.0\n")
-            f.write("\tactions\n")
-            for action in self.actions:
-                    f.write(f"\t({action.identifier})\n")
-            
-            f.write(")\n")
-
-    def action(self, identifier):
-        action = Action(identifier)
-        self.actions.append(action)
-        return action
+        raise NotImplementedError()
     
-    def no_input_behavior(self, name, parameters):
-        self.no_input_behavior = {
-            'Name': name,
-            'Parameters': parameters
-        }
-        
-        return self.no_input_behavior
+    @staticmethod
+    def load(self, path):
+        raise NotImplementedError()
+    
